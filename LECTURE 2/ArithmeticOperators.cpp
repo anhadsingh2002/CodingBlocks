74ea856894373560008ae70f806e506500e60c00
anhadsingh2002 <86299946+anhadsingh2002@users.noreply.github.com>
2022-07-10 16:49:26 +0530

    Add files via upload

---
+++ LECTURE 2/ArithmeticOperators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<<1+2<<endl;
+	cout<<5-2<<endl;
+	cout<<2*4<<endl;
+	cout<<6/3<<endl;
+	cout<<5%3<<endl;
+	cout<<3-5<<endl;
+
+	return 0;
+}
---
+++ LECTURE 2/interest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int p;
+	cin>>p;
+	int r;
+	cin>>r;
+	int t;
+	cin>>t;
+
+	int si=(p*r*t)/100;
+	cout<<"Simple Interest: " <<si << endl;
+
+	return 0;
+}
