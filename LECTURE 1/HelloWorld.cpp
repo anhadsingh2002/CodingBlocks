1c193ea73cd176959afd8da8ebccd0bdfd812e0c
anhadsingh2002 <86299946+anhadsingh2002@users.noreply.github.com>
2022-07-10 16:48:46 +0530

    Add files via upload

---
+++ LECTURE 1/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>        // HEADER FILE
+using namespace std;       // STANDARD NAMESPACE IS BEING USED
+
+int main() {               // START
+
+    cout<<" Hello World";  // PRINT
+    return 0;              // EXIT
+}
+// IOSTREAM--> INPUT & OUTPUT
---
+++ LECTURE 1/Intitialization.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cout << x <<endl;
+
+	//INITIALIZATION: BUCKET BANATE TIME USME VALUE DENA
+	int a = 10;
+
+	//ASSIGNMENT: BUCKET BANI HUI HAI, USSE VALUE DEDO
+	int y;
+	y = 20;  //ASSIGNMENT OPEREATOR
+
+	cout<<y;
+
+	return 0;	
+}
