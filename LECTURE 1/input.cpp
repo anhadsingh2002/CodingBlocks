62f1358b83519725cd5dd9473c6fff503132c75a
anhadsingh2002 <86299946+anhadsingh2002@users.noreply.github.com>
2022-07-10 16:38:40 +0530

    Add files via upload

---
+++ LECTURE 1/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    int r;
+    cin >> n;
+    cin >> r;
+
+    //cout <<"user ne enter kia:";
+    //cout << n;
+    cout <<"user ne enter kia:"<< n <<'\n';
+    cout <<"value of r:"<< r << '\n'; // MULTIPLE BUCKETS
+    
+
+    return 0;
+}
